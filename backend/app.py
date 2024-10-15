372eec1d32dd3eb459f8d10fb168a66eb4cfb86a
Jasmine <91393581+sidhuja@users.noreply.github.com>
2024-10-15 16:43:39 -0700

    supabase client initialize skeleton

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,17 @@
 from flask import Flask
+from supabase import create_client, Client
+from dotenv import load_dotenv 
+import os 
+
+# load env variables from .env file
+# load_dotenv()
+
+# get value of supabase env variables 
+# SUPABASE_URL = os.environ.get("SUPABASE_URL", "")
+# SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "")
+
+# initalize supabase client 
+# supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = Flask(__name__)
 
