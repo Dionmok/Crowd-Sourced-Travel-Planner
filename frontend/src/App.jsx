57c4466d421118d34811bb376a8a9caa18a984cc
Tyson <tysonpederson95@gmail.com>
2024-10-24 20:02:05 -0700

    Reorder App.jsx imports to fix css cascading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
+import "./Reset.css";
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import MyExperiences from "./pages/MyExperiences";
 import CreateExperience from "./pages/CreateExperience";
-import "./Reset.css";
-import "./App.css";
 
 function App() {
   return (
