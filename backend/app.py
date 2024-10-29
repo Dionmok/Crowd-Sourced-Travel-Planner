753466cb522e40d3d5412afcaaa21cd0b88d0f83
Tyson <tysonpederson95@gmail.com>
2024-10-28 18:37:54 -0700

    Uncomment lines to enable supabase connection

--- backend/app.py
+++ backend/app.py
@@ -4,20 +4,22 @@ from dotenv import load_dotenv
 import os 
 
 # load env variables from .env file
-# load_dotenv()
+load_dotenv()
 
 # get value of supabase env variables 
-# SUPABASE_URL = os.environ.get("SUPABASE_URL", "")
-# SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "")
+SUPABASE_URL = os.environ.get("SUPABASE_URL", "")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "")
 
 # initalize supabase client 
-# supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World, from Flask!"
+    response = supabase.table("Users").select("*").execute()
+    print(response)
+    return f"Hello World, from Flask!"
 
 if __name__ == '__main__':
     app.run(debug=True) # enable debug mode 
