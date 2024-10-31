dd01c6474c4c1d9984fb1ae69816eba0e522d557
Jasmine <91393581+sidhuja@users.noreply.github.com>
2024-10-31 13:38:22 -0700

    Created GeolocationInput component for Latitude and Longitude Input for Creating Experience

---
+++ frontend/src/components/GeolocationInput.jsx
@@ -0,0 +1,37 @@
+import React, { useState} from 'react';
+import '../css/GeolocationInput.css';
+
+export default function GeolocationInput({placeholder, id, value="", onChange}){
+    // const [number, setNumber] = useState("");
+    const [error, setError] = useState("");
+    const maxChars = 8;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+    // handle input change
+    const handleChange = (e) => {
+        const value = e.target.value.slice(0, maxChars);
+        onChange(value); //call onChange from props to update parent state
+    };
+
+const leftOverChars = maxChars - value.length;
+
+return(
+    <div>
+        {/* input field*/}
+        <input
+        id={id}
+        type="number"
+        value={value} // use value from props
+        onChange={handleChange}
+        placeholder={placeholder}
+        maxLength={maxChars}
+        required
+        />
+        {/* display error message if input is invalid */}
+        <div className="charCounter">
+            {leftOverChars} characters remaining
+        </div>
+    </div>
+)
+};
+
+
