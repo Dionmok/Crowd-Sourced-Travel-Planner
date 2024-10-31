f90c0770b357ec295ffad96e6f5b8b390adea5e3
Jasmine <91393581+sidhuja@users.noreply.github.com>
2024-10-31 13:44:45 -0700

    Rating components with selection attribute for Create Experience

---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+export default function Rating({value, id, onChange}){
+    // const [rating, setRating] = useState("");
+    
+    const handleChange = (e) => {
+        onChange(e.target.value);
+    }
+
+    return(
+        <select 
+        name="rating" 
+        id={id}
+        value={value}
+        onChange={handleChange}
+        required
+        >   
+            <option value="">Select Rating</option> {/* Default option */}
+            <option value="1">1 Star</option>
+            <option value="2">2 Stars</option>
+            <option value="3">3 Stars</option>
+            <option value="4">4 Stars</option>
+            <option value="5">5 Stars</option>
+      </select>
+    );
+}
