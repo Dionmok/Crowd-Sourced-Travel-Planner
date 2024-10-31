f1f3005ddaf2152ca6d423d1aa78a5757795fb40
Jasmine <91393581+sidhuja@users.noreply.github.com>
2024-10-31 13:53:00 -0700

    Added functionality to handle change of state for all components, and created a experienceData object to submit to backend

--- frontend/src/pages/CreateExperience.jsx
+++ frontend/src/pages/CreateExperience.jsx
@@ -1,43 +1,146 @@
 import NavBar from "../components/NavBar";
 import ImageUpload from "../components/ImageUpload";
-
 import ButtonLink from "../components/ButtonLink";
-import Descrtiption from "../components/Description";
+import TextBox from "../components/TextBox";
 import Keywords from "../components/Keywords";
+import GeolocationInput from "../components/GeolocationInput";
+import Rating from "../components/Rating";
+import { useState } from "react";
 
 export default function CreateExperience() {
+  const [error, setError] = useState("");
+  const [experienceName, setExperienceName] = useState("");
+  const [description, setDescription] = useState("");
+  const [address, setAddress] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [keywords, setKeywords] = useState([]);
+  const [photoURL, setPhotoURL] = useState("");
+  const [rating, setRating] = useState("");
+
+  // handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    console.log("Handling Submit");
+    
+    // check if all required fields are filled
+    if (!experienceName || !description || !address || !latitude || !longitude || !address || keywords.length === 0 || !photoURL || !rating ) {
+      setError("Please fill in all required fields");
+      return;
+    }
+    setError(""); // clear error message
+
+    // create experience object
+    const experienceData = {
+      experience_name: experienceName,
+      description: description,
+      photo: photoURL,
+      latitude: latitude,
+      longitude: longitude,
+      address: address,
+      keywords: keywords,
+      rating: rating,
+      // published: true, 
+      // TODO: create Publish toggle button component to set this value
+      time_created: new Date().toISOString() // unchanged
+    };
+    console.log("Submiting form data:", experienceData);
+
+    // TODO: send data to backend
+
+  };
 
   return (
     <>
       <NavBar />
       <h1>Create Experience page</h1>
-      <div>
-        <h1>Title</h1>
-        <Descrtiption maxChars='60' varient="description-title" placeholder="e.g., Davis Farmers Market..."/>
-      </div>
-      <div>
-        <h1>Description</h1>
-        <Descrtiption maxChars='200' varient="description-default" placeholder="Enter experience description here..."/>
-      </div>
-      <div>
-        <h1>Geolocation</h1>
-        <div class='form-row'>
-          <h1>Latitude</h1>
-          <Descrtiption maxChars='8' varient="description-geolocation" placeholder="38.8951"/>
-          <h1>Longitude</h1>
-          <Descrtiption maxChars='8' varient="description-geolocation" placeholder="-77.0364"/>
+      <form onSubmit={handleSubmit} id='createExperience'>
+        <div className="title-container">
+          <h1>Title</h1>
+          <TextBox 
+            name='experienceName' 
+            id='experienceName'
+            maxChars='60' 
+            varient="description-title" 
+            placeholder="e.g., Davis Farmers Market..." 
+            value={experienceName}
+            onChange={setExperienceName}
+            required/>
         </div>
-      </div>
-      <div>
-        <h1>Keywords</h1>
-        <Keywords />
-      </div>
-      <div>
-        <h1>Image</h1>
-        <ImageUpload />
-      </div>
-      <ButtonLink varient="button-add" buttonName="Save" routeTo="/myExperiences" />
-      <ButtonLink varient="button-back" buttonName="Cancel" routeTo="/myExperiences" />
+        <div className="description-container">
+          <h1>Description</h1>
+          <TextBox 
+            name='description'
+            id='description' 
+            maxChars='200' 
+            varient="description-default" 
+            placeholder="Enter experience description here..." 
+            value={description} 
+            onChange={setDescription}
+            required />
+        </div>
+        <div className="location-container">
+          <h1>Address</h1>
+          <TextBox 
+            name='address' 
+            id='address'
+            maxChars='100' 
+            varient="description-title" 
+            placeholder="Enter location here..." 
+            value={address} 
+            onChange={setAddress}
+            required/>
+          <h1>Geolocation</h1>
+          <div className='geolocation-input-container'>
+            <h1>Latitude</h1>
+              <GeolocationInput 
+                name='latitude' 
+                id='latitude'
+                placeholder="38.8951" 
+                value={latitude} 
+                onChange={setLatitude} />
+            <h1>Longitude</h1>
+              <GeolocationInput 
+              name='longitude' 
+              id='longitude'
+              placeholder="-77.0364"
+              value={longitude} 
+              onChange={setLongitude}/>
+          </div>
+        </div>
+        <div className="keywords-container">
+          <h1>Keywords</h1>
+          <Keywords 
+            name='keywords'
+            value={keywords}
+            onChange={setKeywords} />
+        </div>
+        <div className="image-container">
+          <h1>Image</h1>
+          <ImageUpload 
+            name='image'
+            id='imageUpload'
+            value={photoURL}
+            onChange={setPhotoURL} />
+        </div>
+        <div className="rating-container">
+          <h1>Rating</h1>
+          <Rating 
+            name='rating'
+            id='createExperienceRating'
+            value={rating}
+            onChange={setRating} />
+        </div>
+        <ButtonLink type="submit" varient="button-add" buttonName="Save" />
+        <ButtonLink varient="button-back" buttonName="Cancel" routeTo="/myExperiences" />
+        
+        {/* Error Message Display */}
+        {error && (
+          <div>
+            {error}
+          </div>
+        )}
+      </form>
     </>
   );
 }
