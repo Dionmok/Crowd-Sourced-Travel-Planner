1ad8e19c0d521145ce80667a84c462105b1f0a92
Jasmine <91393581+sidhuja@users.noreply.github.com>
2024-11-04 23:53:30 -0800

    Added fetching of existing keywords, drop down selection of them, and option to input new keywords

--- frontend/src/components/Keywords.jsx
+++ frontend/src/components/Keywords.jsx
@@ -1,12 +1,37 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect} from 'react';
 
 export default function Keywords({value=[], onChange}){
     const [keyword, setKeyword] = useState("");
 
+    const [existingKeywords, setExistingKeywords] = useState([]);
+
+    // fetch existing keywords from the backend
+    useEffect(() => {
+        const fetchKeywords = async () => {
+            try {
+                const response = await fetch(`http://127.0.0.1:5000/get_keywords`);
+                if (response.ok) {
+                    const data = await response.json();
+                    setExistingKeywords(data.map(keyword => keyword.keyword)); // Assume the keyword is in keyword field
+                    // setExistingKeywords(data);
+                } else {
+                    console.error('Failed to fetch keywords');
+                }
+            }
+            catch(error){
+                console.error(error);
+            }
+        }
+        fetchKeywords();
+    }
+    , []);
+    console.log(existingKeywords);
+
+
     // add a keyword to the list
     const handleAdd = () => {
-        // check if the keyword is not empty
-        if (keyword.trim()) {
+        // check if the keyword is not empty and not already in the list
+        if (keyword.trim() && !value.includes(keyword)) {
             onChange([...value, keyword]); // update parent state with new keyword list
             setKeyword(""); 
         }
@@ -20,12 +45,23 @@ export default function Keywords({value=[], onChange}){
     return(
         <>
             <div className='keyword-input-container'>  
+                <select
+                    value={keyword}
+                    onChange={(e) => setKeyword(e.target.value)}
+                    >
+                    <option value="">Select a keyword</option>
+                    {existingKeywords.map((existingKeyword, index) => (
+                        <option key={index} value={existingKeyword}>
+                            {existingKeyword}
+                        </option>
+                    ))}
+                    </select>
                 <div className='keyword-input'>
                     <input
                         type="text"
                         value={keyword}
                         onChange={(e) => setKeyword(e.target.value)} // update keyword input
-                        placeholder="Add keyword"
+                        placeholder="Or add new keyword"
                     />
                     <button type='button' onClick={handleAdd}>Add</button>
                 </div>
