40ccca239ea841131eb001110e25e933ed5869e2
Jasmine <91393581+sidhuja@users.noreply.github.com>
2024-11-04 17:57:24 -0800

    Added trips-contianer styling and mapping to MyExperiences page

--- frontend/src/pages/MyExperiences.jsx
+++ frontend/src/pages/MyExperiences.jsx
@@ -1,13 +1,46 @@
+import { useState,  useEffect } from "react";
 import NavBar from "../components/NavBar";
 import ButtonLink from "../components/ButtonLink";
+import ExperienceTile from "../components/ExperienceTile";
+import '../css/MyTrips.css';
 
-export default function MyExperiences() {
+export default function MyExperiences({userId}) {
+  const [experiences, setExperiences] = useState([]);
+
+  useEffect(() => {
+    const fetchExperiences = async () => {
+      try {
+        const response = await fetch(`http://127.0.0.1:5000/experiences/123`); // Hardcoded for testing; replace with dynamic user_id fetching logic
+        if (!response.ok){
+          throw new Error('Failed to fetch experiences');
+        }
+        const data = await response.json();
+        console.log(data);
+        setExperiences(data)
+      }
+      catch(error){
+        console.error(error)
+      }
+    }
+    fetchExperiences();
+  }, [userId]);
+
   return (
     <>
       <NavBar />
       <h1>My Experiences page</h1>
       <ButtonLink varient="button-add" buttonName="+ Experience" routeTo="/createExperience" />
-
+      <div className="trips-container">
+        {experiences.length == 0 ? (
+          <p>There are no experiences created.</p>
+        ) : (
+          <div>
+            {experiences.map((experience) => (
+              <ExperienceTile key={experience.experience_id} experience={experience} userId={userId} />
+            ))}
+          </div >
+        )}
+      </div>
     </>
   );
 }
