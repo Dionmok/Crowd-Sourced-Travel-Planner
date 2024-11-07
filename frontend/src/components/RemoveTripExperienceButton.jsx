cad0b3679c02d9a0d988fb3f4482a3d27e3c88a2
Joshua <joshua.lena497@gmail.com>
2024-11-06 20:31:05 -0700

    Add RemoveTripExperienceButton component

---
+++ frontend/src/components/RemoveTripExperienceButton.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export default function DeleteTripExperienceButton({ tripId, experienceId, onExperienceRemoved }){
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(false);
+
+    const handleDelete = async () => {
+        // Pop-up window confirms if user wants to remove the expereince from a trip
+        const confrimDelete = window.confirm("Are you sure you remove this experience from the trip?")
+        if(!confrimDelete) {
+            return;
+        } 
+
+        try {
+            // Makes a DELETE request
+            const response = await fetch(`http://127.0.0.1:5000/trip/${tripId}/experience/${experienceId}`, {
+                method: 'DELETE',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to remove the experience from the trip');
+        }
+
+        onExperienceRemoved(experienceId)
+        setSuccess(true);
+        onTripDeleted(tripId);
+    } catch (err) {
+        setError(err.message);
+    }
+};
+
+return (
+    <div>
+        <button className="button-delete" onClick={handleDelete}>
+            Remove
+        </button>
+    </div>
+    ); 
+};
