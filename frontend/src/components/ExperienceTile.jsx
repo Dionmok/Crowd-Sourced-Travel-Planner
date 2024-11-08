e6982cff82a5b1f987b982f8e0d8741bae576ad1
Jasmine <91393581+sidhuja@users.noreply.github.com>
2024-11-07 18:17:13 -0800

    Added styling for ExperienceTiles components

--- frontend/src/components/ExperienceTile.jsx
+++ frontend/src/components/ExperienceTile.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import EditExperienceButton from '../components/EditExperienceButton';
 import DeleteExperienceButton from '../components/DeleteExperienceButton';
-import '../css/TripTile.css';
+import '../css/ExperienceTile.css';
 
 export default function ({ experience, userId, onExperienceDeleted }){
     const navigate = useNavigate();
@@ -12,11 +12,41 @@ export default function ({ experience, userId, onExperienceDeleted }){
     };
 
     return(
-        <div className="trip-tile">
-            <h2 onClick={handleClick}>{experience.experience_name}</h2>
-            <DeleteExperienceButton experienceId={experience.experience_id} userId={userId} onExperienceDeleted={onExperienceDeleted} />
-            <EditExperienceButton experience={experience}/>
+        <div onClick={handleClick} className="experience-tile">
+            <div className='image'>
+                <img src={experience.photo} alt="Experience" />
+            </div>
+            <div className="experience-details">
+                <div className='title'>
+                    <h2>{experience.experience_name}</h2>
+                </div>
+                <div className='rating'>
+                    <p>Rating:{experience.rating}</p>
+                </div>
+                {/* TODO: Add keywordss  */}
+                {/* <div className='keywords'>
+                    <p>Keywords: {experience.keywords.join(', ')}</p>
+                </div> */}
+                <div className='address'>
+                    <p>Address: {experience.address}</p>
+                </div>
+                <div className='description'>
+                    <p>Description: {experience.description}</p>
+                </div>
+            </div>
 
+            <div className='button-edit-container'>
+                <div className='edit'>
+                    <EditExperienceButton experience={experience}/>
+                </div>
+                <div className='delete'>
+                    <DeleteExperienceButton experienceId={experience.experience_id} userId={userId} onExperienceDeleted={onExperienceDeleted} />
+                </div>
+                
+                
+            </div>
+        
+            
         </div>
     );
 };
