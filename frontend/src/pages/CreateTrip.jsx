454c3c055a1b440880e7ef169b2128c1c50cbbea
Dion Mokhtari <dionmokhtari@Dions-MacBook-Air.local>
2024-11-11 15:37:17 -0800

    CSS

--- frontend/src/pages/CreateTrip.jsx
+++ frontend/src/pages/CreateTrip.jsx
@@ -33,8 +33,8 @@ export default function CreateTrip() {
       <NavBar />
       <div>
         <label>
-          <h2 className="create-title">Trip Name</h2>
-          <div className="create-title-container">
+          <h2 className='create-title'>Trip Name</h2>
+          <div className='create-title-container'>
           <Description
             maxChars={60}
             variant="description-title"
