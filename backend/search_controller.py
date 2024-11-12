01713c7a3b05c4dd7c633162f4460e3b1705f487
Joshua <joshua.lena497@gmail.com>
2024-11-11 18:28:52 -0700

    Move /experiences route and search_experiences() function to search_controller.py

---
+++ backend/search_controller.py
@@ -0,0 +1,33 @@
+from __main__ import app, supabase
+from flask import request, jsonify
+
+@app.route('/experiences', methods=['GET'])
+def search_experiences():
+    location = request.args.get('location', '').strip()
+    keywords = request.args.get('keywords', '').strip().split(',')
+
+    query = supabase.table('Experiences').select('*').eq('published', True)
+    if location:
+        query = query.ilike('address', f"%{location}%")
+
+    if keywords and keywords[0]:
+        keyword_ids_response = supabase.table('Keywords').select('keyword_id').in_('keyword', keywords).execute()
+
+        if not keyword_ids_response.data:
+            return jsonify({"error": "No experiences found matching the keywords"}), 404
+
+        keyword_ids = [item['keyword_id'] for item in keyword_ids_response.data]
+        experience_ids_response = supabase.table('Experience_Keywords').select('experience_id').in_('keyword_id', keyword_ids).execute()
+
+        if not experience_ids_response.data:
+            return jsonify({"error": "No experiences found matching the keywords"}), 404
+
+        experience_ids = [item['experience_id'] for item in experience_ids_response.data]
+        query = query.in_('experience_id', experience_ids)
+
+    response = query.execute()
+
+    if response.data:
+        return jsonify(response.data), 200
+    else:
+        return jsonify({"error": "No experiences found"}), 404
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import NavBar from "../components/NavBar";
 import SearchBar from "../components/SearchBar";
-import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   return (
