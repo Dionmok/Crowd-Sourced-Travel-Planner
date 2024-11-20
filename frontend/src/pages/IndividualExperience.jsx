6234cafc8176a9115f57aab05c32b05f3c8169fb
Jasmine <91393581+sidhuja@users.noreply.github.com>
2024-11-20 12:57:32 -0800

    Added css styling for individual experinece

--- frontend/src/pages/IndividualExperience.jsx
+++ frontend/src/pages/IndividualExperience.jsx
@@ -4,6 +4,7 @@ import NavBar from "../components/NavBar";
 import ButtonLink from "../components/ButtonLink";
 import EditExperienceButton from "../components/EditExperienceButton";
 import DeleteExperienceButton from "../components/DeleteExperienceButton";
+import AddExperienceToTripButton from "../components/AddExperienceToTripButton";
 import '../css/IndividualExperience.css';
 
 export default function IndividualExperience() {
@@ -118,6 +119,10 @@ export default function IndividualExperience() {
       setRating(data.rating);
     };
 
+    const handleGoBack = () => {
+      navigate(-1);
+    }
+
     return (
         <>
           <NavBar />
@@ -148,6 +153,12 @@ export default function IndividualExperience() {
                     </div>
                     }
                   </div>
+                  {localStorage.getItem("token") && experience.user_id === JSON.parse(atob(localStorage.getItem("token").split(".")[1]))
+                    .sub && <EditExperienceButton experience={experience} />}
+                  {localStorage.getItem("token") && experience.user_id === JSON.parse(atob(localStorage.getItem("token").split(".")[1]))
+                    .sub && <DeleteExperienceButton experience={experience} />}
+                  <ButtonLink varient="button-back" buttonName="Back" routeTo="/myExperiences" />
+                  {/* <button onClick={handleGoBack}>GO BACK</button> */}
                 </div>
                 {/* right side  */}
                 <div className="right-container">
@@ -176,18 +187,12 @@ export default function IndividualExperience() {
                           ))}
                       </div>
                   </div>
-                  
+                  <div className="add-to-trip-container">
+                    <AddExperienceToTripButton experience={experience} />
+                  </div>
                 </div>
               </div>
           </div>
-
-            {localStorage.getItem("token") && experience.user_id === JSON.parse(atob(localStorage.getItem("token").split(".")[1]))
-              .sub && <EditExperienceButton experience={experience} />}
-            {localStorage.getItem("token") && experience.user_id === JSON.parse(atob(localStorage.getItem("token").split(".")[1]))
-              .sub && <DeleteExperienceButton experience={experience} />}
-            {/* TODO: Add ExperinceTrip button  */}
-            <ButtonLink varient="button-back" buttonName="Back" routeTo="/myExperiences" />
-            
             {/* Error Message Display */}
             {error && (
               <div>
